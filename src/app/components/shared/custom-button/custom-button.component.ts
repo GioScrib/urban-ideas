9ba3d67d5a4005586c7ace2edc9551dea517927f
U0N7808 <giovanni.scribanobasile@esternibisp.com>
2025-10-16 17:57:27 +0200

    aggiunti: dialog per creare nuovo utente componente button riutilizzabile

---
+++ src/app/components/shared/custom-button/custom-button.component.ts
@@ -0,0 +1,27 @@
+import {Component, input, output} from '@angular/core';
+import {MatFabButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-custom-button',
+  standalone: true,
+  imports: [
+    MatFabButton,
+    MatIcon
+  ],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss'
+})
+export class CustomButtonComponent {
+
+  matIconName = input<string>('');
+  matButtonContent = input<string>('');
+  backGroundColor = input<string>('');
+  textColor = input<string>('');
+  clicked = output<void>();
+
+  onClick() {
+    console.log('custom button' + this.matIconName + 'says clicked');
+    this.clicked.emit();
+  }
+}
---
+++ src/app/components/user-list/create-user-dialog/create-user-dialog.component.ts
@@ -0,0 +1,44 @@
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatDialogRef} from '@angular/material/dialog';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {CustomButtonComponent} from '../../shared/custom-button/custom-button.component';
+
+@Component({
+  selector: 'app-create-user-dialog',
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatSelect,
+    MatOption,
+    CustomButtonComponent,
+  ],
+  templateUrl: './create-user-dialog.component.html',
+  styleUrl: './create-user-dialog.component.scss'
+})
+export class CreateUserDialogComponent {
+
+  private dialogRef = inject(MatDialogRef<CreateUserDialogComponent>);
+  private fb = inject(FormBuilder);
+
+  newUserForm: FormGroup = this.fb.group({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    gender: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),
+  });
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  submitData(): void {
+    if(this.newUserForm.invalid) {
+      return
+    }
+    this.dialogRef.close(this.newUserForm.value);
+  }
+}
