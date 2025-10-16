c8fbddcc379e1b0421d01903cdd12ed0264b7a75
U0N7808 <giovanni.scribanobasile@esternibisp.com>
2025-10-16 15:23:19 +0200

    migliorato layout della userCard - non definitivo

--- src/app/components/user-list/user-card/user-card.component.ts
+++ src/app/components/user-list/user-card/user-card.component.ts
@@ -1,27 +1,26 @@
-import {Component, input, signal} from '@angular/core';
+import {Component, input, signal, inject} from '@angular/core';
+import {MatFabButton} from '@angular/material/button';
+
 import {
   MatAccordion,
   MatExpansionPanel,
   MatExpansionPanelDescription,
   MatExpansionPanelHeader, MatExpansionPanelTitle
 } from '@angular/material/expansion';
-import {MatIconButton} from '@angular/material/button';
 import {User} from '../../../shared/user';
-import {MatIcon} from '@angular/material/icon';
-import {inject} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {timestamp} from 'rxjs';
+import {MatIcon} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-user-card',
   imports: [
     MatAccordion,
     MatExpansionPanel,
-    MatExpansionPanelDescription,
     MatExpansionPanelHeader,
     MatExpansionPanelTitle,
     MatIcon,
-    MatIconButton
+    MatFabButton,
   ],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
@@ -38,6 +37,5 @@ export class UserCardComponent {
     this.userImg = `https://avatar.iran.liara.run/public/girl?${timestamp}`;
   }
 
-  panelOpenState1 = signal<boolean>(false);
   panelOpenState2 = signal<boolean>(false);
 }
