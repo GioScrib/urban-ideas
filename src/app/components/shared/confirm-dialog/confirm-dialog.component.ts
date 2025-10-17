b37c67726d19df717c3a0063a83e693b4e53129f
U0N7808 <giovanni.scribanobasile@esternibisp.com>
2025-10-17 17:30:45 +0200

    Creato componente confirm-dialog riutilizzabile

---
+++ src/app/components/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import {Component, inject, input} from '@angular/core';
+import {CustomButtonComponent} from '../custom-button/custom-button.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+export interface ConfirmDialogData {
+  title: string;
+  content: string;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [
+    CustomButtonComponent
+  ],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+
+  private ref = inject(MatDialogRef<ConfirmDialogComponent>);
+  data = inject<ConfirmDialogData>(MAT_DIALOG_DATA);
+
+  close(v: boolean): void {
+    //il metodo close emette un signal di output
+    this.ref.close(v);
+  }
+}
